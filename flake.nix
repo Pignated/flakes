f51811acc21bc4c10181886efa2462ccd154873f
cobbn <npcobb2003@gmail.com>
2026-02-08 21:14:50 -0600

    remove unneccesary line

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
             {
                 devShells.java = pkgs.callPackage ./java/default.nix {};
                 devShells.C = pkgs.callPackage ./C/default.nix {};
-                C = pkgs.callPackage ./C/default.nix {};
             }
         );
 }
