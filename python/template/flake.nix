cdc2efff3905f1369f4e7fa89b7ea4959a8dd1bb
cobbn <npcobb2003@gmail.com>
2026-03-12 10:04:40 -0500

    fixed flake again for python

--- python/template/flake.nix
+++ python/template/flake.nix
@@ -4,16 +4,23 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    monorepo.url = "github:pignated/flakes";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, monorepo }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         # This imports the default.nix that is already in this folder
-        pythonShell = pkgs.callPackage ./default.nix {};
+        pythonShell = monorepo.devShells.x86_64-linux.python;
+        pythonExtras = pkgs.python3.withPackages (ps: with ps; [
+            #add additional packages here
+        ]);
       in
       {
-        devShells.default = pythonShell;
+        devShells.default = pkgs.mkShell {
+            inputsFrom = [pythonShell];
+            buildInputs = [pythonExtras];
+                }
       });
 }
