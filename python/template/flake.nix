9cb68c951080f58dcc16e333e66711930def47ae
cobbn <npcobb2003@gmail.com>
2026-04-06 19:39:50 -0500

    rtaogasgasjg

--- python/template/flake.nix
+++ python/template/flake.nix
@@ -7,25 +7,33 @@
     monorepo.url = "github:pignated/flakes";
   };
 
+  # The 'outputs' must be a function that returns an attribute set
   outputs = { self, nixpkgs, flake-utils, monorepo }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-  pkgs = import nixpkgs { inherit system; };
-        pythonBase= pkgs.callPackage "${monorepo}/python/default.nix" {};
-        pythonEnv = 
-                    (pkgs.python3.withPackages (ps: with ps;
-            pythonBase.packages ++ [
-                    #add additional packages here
-            ]));
+        pkgs = import nixpkgs { inherit system; };
+        
+        # Import the monorepo logic
+        pythonBase = import "${monorepo}/python/default.nix" { inherit pkgs; };
+        
+        # Build the environment by calling the library function from the monorepo
+        pythonEnv = pkgs.python3.withPackages (ps: 
+          (pythonBase.pythonLibraries ps) ++ [
+            # ps.requests  <-- Add extra libs here
+          ]
+        );
       in
       {
         devShells.default = pkgs.mkShell {
-            inherit (pythonBase) env;
-            packages = [
-                        pythonEnv
-                        pkgs.basedpyright
-                    ];
-            shellHook = pythonBase.shellHook;
+          # Spread the environment variables so mkShell sees them
+          inherit (pythonBase.env) USING_PYTHON;
+          inherit (pythonBase) shellHook;
+
+          packages = [
+            pythonEnv
+            pkgs.basedpyright
+          ];
         };
       });
 }
+
